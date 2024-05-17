aad59161093ab2148fc3f3effea119c4dfabf63a
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-17 15:28:11 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -6,3 +6,23 @@ function processArray(arr) {
 const result = processArray([1, 2, 3, 4]);
 console.log(result);
 // This gives us a result of [3, 4, 9, 16].
+
+
+// Task 2, manipulating the result with the arrayManipulation.js
+
+function formatArrayStrings(stringArr, numArr) {
+    return stringArr.map((str, index) => {
+        if (numArr[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+        });
+    }
+
+// for example
+const strings = ['King', 'fred', 'sheep', 'cat'];
+const numbers = processArray([1, 2, 3, 4]);
+const formattedStrings = formatArrayStrings(strings, numbers); 
+console.log(formattedStrings);
+// the formatted strings gives us [ 'king', 'FRED', 'sheep', 'CAT' ]

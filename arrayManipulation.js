56373f3d9015cf32a7e246dfd2850b5ff2352995
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-17 15:49:46 +0000

    Changes in the arrayManipulation.js file and creation of userInfo.js file

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -27,35 +27,3 @@ const formattedStrings = formatArrayStrings(strings, numbers);
 console.log(formattedStrings);
 // the formatted strings gives us [ 'king', 'FRED', 'sheep', 'CAT' ]
 
-
-// Task 3 , original and modified names with userInfo.js
-
-let id;
-id = 1;
-
-function createUserProfiles(names, modifiedNames) {
-    return names.map(( name, index) => {
-        return {
-            originalName: name,
-            modifiedNames: modifiedNames[index],
-            id: id++,
-        };
-    });
-}
-
-// for example
-// Using the names and modified names in Task 2
-const number = [1, 2, 3, 4];
-const processedNumber = processArray(number);
-const names = ['King', 'fred', 'sheep', 'cat']
-const modifiedNames = formatArrayStrings(names, processedNumber)
-const userProfiles = createUserProfiles(names, modifiedNames);
-console.log(userProfiles);
-
-// The User profile prints out like this 
-// [
-//   { originalName: 'King', modifiedNames: 'king', id: 1 },
-//   { originalName: 'fred', modifiedNames: 'FRED', id: 2 },
-//   { originalName: 'sheep', modifiedNames: 'sheep', id: 3 },
-//   { originalName: 'cat', modifiedNames: 'CAT', id: 4 }
-// ]

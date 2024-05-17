6cb044b76eec84dde8101489d5ce41e84aec5598
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-17 16:07:15 +0000

    end of task 4

--- userInfo.js
+++ userInfo.js
@@ -13,3 +13,20 @@ function createUserProfiles(names, modifiedNames) {
     });
 }
 
+// for example
+// Using the names and modified names in Task 2
+const number = [1, 2, 3, 4];
+const processedNumber = processArray(number);
+const names = ['King', 'fred', 'sheep', 'cat']
+const modifiedNames = formatArrayStrings(names, processedNumber)
+const userProfiles = createUserProfiles(names, modifiedNames);
+console.log(userProfiles);
+
+
+// The User profile prints out like this 
+// [
+//   { originalName: 'King', modifiedNames: 'king', id: 1 },
+//   { originalName: 'fred', modifiedNames: 'FRED', id: 2 },
+//   { originalName: 'sheep', modifiedNames: 'sheep', id: 3 },
+//   { originalName: 'cat', modifiedNames: 'CAT', id: 4 }
+// ]

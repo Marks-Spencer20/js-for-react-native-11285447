1b8e9bb22e71e5ccd0b306e3300259f6a388a6b1
Marks-Spencer20 <javisfrimpong.2003@gmail.com>
2024-05-17 15:23:26 +0000

    Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,8 @@
+// Task 1
+function processArray(arr) {
+    return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
+}
+// for example
+const result = processArray([1, 2, 3, 4]);
+console.log(result);
+// This gives us a result of [3, 4, 9, 16].
